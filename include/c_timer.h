393b92b85896f23baf0f3888d3dbf5ad0d440d09
rick <rick@ricks-MacBook-Air.local>
2022-05-28 20:09:38 -0400

    tighten up output code

--- include/c_timer.h
+++ include/c_timer.h
@@ -1,80 +1,37 @@
 #pragma once
 #ifndef C_TIMER_H
 #define C_TIMER_H
+#include <assert.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+#include <time.h>
 /////////////////////////////////
 #define NANOSECONDS_PER_SECOND     1000000000
 #define MICROSECONDS_PER_SECOND    1000000
 #define MILLISECONDS_PER_SECOND    1000
 /////////////////////////////////
-#include "../include/c_timer_types.h"
-#include "../include/includes.h"
-/////////////////////////////////
-static struct timespec ct_start_time  = { 0, 0 };
-static ct_node         *ct_stack      = NULL;
-static ct_unit         ct_output_unit = ct_SECONDS;
 
+typedef enum ct_unit {
+  ct_SECONDS,
+  ct_MILLISECONDS,
+  ct_MICROSECONDS,
+  ct_NANOSECONDS
+} ct_unit;
+typedef struct ct_node {
+  struct timespec start_time;
+  struct ct_node  *next;
+} ct_node;
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 
-/////////////////////////////////
-static void ct_set_unit(ct_unit unit) {
-  ct_output_unit = unit;
-}
+#define OK(MSG)    { fprintf(stderr, "  \e[32m\u2713 \e[90mOK- %s\e[0m\n", MSG); }
 
-
-static void ct_start(char const *msg) {
-  ct_node *new_node = (ct_node *)malloc(sizeof(ct_node));
-
-  assert(new_node);
-
-  clock_gettime(CLOCK_REALTIME, &(new_node->start_time));
-  new_node->next = ct_stack;
-
-  ct_stack = new_node;
-}
-
-
-char * ct_stop(const char *msg) {
-  static struct timespec end_time;
-  static double          elapsed_time;
-  static char const      *unit = "s";
-
-  assert(ct_stack);
-  memcpy(&ct_start_time, &(ct_stack->start_time), sizeof(struct timespec));
-
-  ct_node *old_top = ct_stack;
-
-  ct_stack = ct_stack->next;
-  free(old_top);
-
-  clock_gettime(CLOCK_REALTIME, &end_time);
-  elapsed_time = (end_time.tv_nsec - ct_start_time.tv_nsec) + (end_time.tv_sec - ct_start_time.tv_sec) * NANOSECONDS_PER_SECOND;
-
-  switch (ct_output_unit) {
-  case ct_SECONDS:
-    unit          = "s";
-    elapsed_time /= NANOSECONDS_PER_SECOND;
-    break;
-  case ct_MILLISECONDS:
-    unit          = "ms";
-    elapsed_time /= MILLISECONDS_PER_SECOND;
-    break;
-  case ct_MICROSECONDS:
-    unit          = "us";
-    elapsed_time /= MICROSECONDS_PER_SECOND;
-    break;
-  case ct_NANOSECONDS:
-    unit = "ns";
-    break;
-    /* elapsed_time is already sorted. */
-  }
-
-  char *buf = malloc(1024);
-
-  if (strlen(msg) < 1) {
-    sprintf(buf, "%f%s", elapsed_time, unit);
-  }else{
-    sprintf(buf, "%s: %f%s", msg, elapsed_time, unit);
-  }
-  return(strdup(buf));
-} // ct_stop
 #endif
-
